ce32d1676ea69c30fbca53df3c2cfb482c30837b
szymon <maciazekbiznes@gmail.com>
2020-07-23 15:25:17 +0200

    Add data

---
+++ my-app/src/data/foundations.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        "id": 1,
+        "name": 'Fundacja "Dbam o Zdrowie"',
+        "goal": 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+        "items": 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        "id": 2,
+        "name": 'Fundacja "Dla dzieci"',
+        "goal": 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
+        "items": 'ubrania, meble, zabawki'
+    },
+    {
+        "id": 3,
+        "name": 'Fundacja "Bez domu"',
+        "goal": 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
+        "items":'ubrania, jedzenie, ciepłe koce'
+    },
+    {
+        "id": 4,
+        "name": 'Fundacja "Lorem-1"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items":'ubrania, jedzenie, meble, zabawki'
+    },
+    {
+        "id": 5,
+        "name": 'Fundacja "Lorem-2"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items":'ubrania, meble, zabawki'
+    },
+    {
+        "id": 6,
+        "name": 'Fundacja "Lorem-3"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items":'ubrania, meble, zabawki'
+    },
+    {
+        "id": 7,
+        "name": 'Fundacja "Lorem-4"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items":'akcesoria, karma, zabawki, koce'
+    },
+    {
+        "id": 8,
+        "name": 'Fundacja "Lorem-5"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items":'ubrania, meble, zabawki'
+    },
+    {
+        "id": 9,
+        "name": 'Fundacja "Lorem-6"',
+        "goal": 'Cel i misja: Lorem ipsum dolor sit amet, qui posse idque mnesarchum..',
+        "items": 'akcesoria, karma, sianko'
+    },
+];
---
+++ my-app/src/data/organizations.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        "id": 1,
+        "name": 'Organizacja "Lorem-1"',
+        "goal": 'Flank shank ham hock pork loin. Tail sirloin beef meatball',
+        "items": 'RTV, AGD, ubrania'
+    },
+    {
+        "id": 2,
+        "name": 'Organizacja "Lorem-2"',
+        "goal": 'Beef ribs fatback kielbasa capicola brisket. Bacon strip steak .',
+        "items": 'ubrania,zabawki,AGD'
+    },
+    {
+        "id": 3,
+        "name": 'Organizacja "Lorem-3"',
+        "goal": 'Bacon ipsum dolor amet ball tip brisket fatback picanha.',
+        "items":'ubrania,koce,Art.gospodarstwa domowego'
+    },
+    {
+        "id": 4,
+        "name": 'Organizacja "Lorem-4"',
+        "goal": 'Turducken pork loin doner chuck pork belly ham hock',
+        "items":'agd,rtv,zabawki,koce'
+    },
+    {
+        "id": 5,
+        "name": 'Organizacja "Lorem-5"',
+        "goal": 'Swine picanha brisket, filet mignon bacon pig kielbasa corned beef shank.',
+        "items":'rtv,agd,ubrania,'
+    },
+    {
+        "id": 6,
+        "name": 'Organizacja "Lorem-6"',
+        "goal": 'ail ribeye shoulder sausage, brisket shankle filet mignon hamburger.',
+        "items":'ubrania,karma dla zwierzat, art.dla zwierzat'
+    }
+];
