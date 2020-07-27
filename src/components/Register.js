dd833fb68d8602d56f5cad91eb5767c97d493672
szymon <maciazekbiznes@gmail.com>
2020-07-27 15:59:21 +0200

    Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import {Link as Scroll} from "react-scroll";
+import Decoration from "../assets/Decoration.svg";
 
 export const Register = () => {
+
+
     return (
         <>
+            <div id='Header' className={"row"}>
+                <div className={"col-5"}/>
+                <div className={"col-7"}>
+                    <div className={'login__box'}>
+                        <Link className={'login'} to={'/login'} href={'#'}>Zaloguj</Link>
+                        <Link className={'login'} to={'/register'} href={'#'}>Załóż konto</Link>
+                    </div>
+                    <ul className={'nav__box'}>
+                        <Scroll to={'Header'} smooth={true} duration={1000} className={'nav__box-element'}>Start</Scroll>
+                        <Scroll to={'ThreeColumns'} smooth={true} duration={1000} className={'nav__box-element'}>O co chodzi?</Scroll>
+                        <Scroll to={'About'} smooth={true} duration={1000} className={'nav__box-element'}>O nas</Scroll>
+                        <Scroll to={'WeHelp'} smooth={true} duration={1000} className={'nav__box-element'}>Fundacja i organizacje</Scroll>
+                        <Scroll to={'Contact'} smooth={true} duration={1000} className={'nav__box-element'}>Kontakt</Scroll>
+                    </ul>
+                </div>
 
-            <div className={"testowa"}>REGISTER</div>
-
+            </div>
+            <div className={'row'}>
+                <div className={'login__container col-12'}>
+                    <h2 className={'login__container-title'}>Załóż konto</h2>
+                    <img src={Decoration} alt={'Decoration'}/>
+                    <form>
+                        <div className={'login__container-box register__box'}>
+                            <div className={'login__container-inputs'}>
+                                <label className={'login__label login__label-email'}>Email</label>
+                                <input className={'login__input'} type={'email'}/>
+                                <p className={'login__warning'}>test</p>
+                                <label className={'login__label'}>Hasło</label>
+                                <input className={'login__input'} type={'password'}/>
+                                <p className={'login__warning'}>test</p>
+                                <label className={'login__label'}>Powtórz Hasło</label>
+                                <input className={'login__input'} type={'password'}/>
+                                <p className={'login__warning'}>test</p>
+                            </div>
+                        </div>
+                        <div className={'login__button-box'}>
+                            <Link className={'login'} to={'/register'} href={'#'}>Załóż konto</Link>
+                            <input className={'login__submit'} type={'submit'} value={'Zaloguj się'}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
-
-
     )
 }
