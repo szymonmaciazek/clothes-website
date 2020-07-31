c7697632a5f929d2d85c7e04029f57c39d56fab3
szymon <maciazekbiznes@gmail.com>
2020-07-31 14:57:53 +0200

    HandoverStep3

--- src/components/HandoverForm3.js
+++ src/components/HandoverForm3.js
@@ -13,11 +13,32 @@ export const HandoverForm3 = () => {
                     <form className={'steps__form'}>
                         <p className={'steps__counter'}>3/4</p>
                         <h2 className={'steps__form-title'}>Lokalizacja:</h2>
-                        <div>
+
                             <div className={'steps__select-box'}>
                                 <Select title={'— wybierz —'} items={['Warszawa', 'Poznań', 'Kraków', 'Wrocław']} />
                             </div>
-                        </div>
+
+                            <div className={"beneficiary__container"}>
+                                <h3 className={"beneficiary__title"}>Komu chcesz pomóc?</h3>
+                                <ul className={"beneficiary__list"}>
+                                    <li className={"beneficiary__element"}>dzieciom</li>
+                                    <li className={"beneficiary__element"}>samotnym matkom</li>
+                                    <li className={"beneficiary__element"}>bezdomnym</li>
+                                    <li className={"beneficiary__element"}>niepełnosprawnym</li>
+                                    <li className={"beneficiary__element"}>osobom starszym</li>
+                                </ul>
+                            </div>
+                            <div className={"beneficiary__organization-container"}>
+                                <label className={"beneficiary__label"}>
+                                    Wpisz nazwę konkretnej organizacji (opcjonalne)
+                                </label>
+                                <input className={"beneficiary__input"} type={"text"}/>
+                            </div>
+
+                            <div className={'step2__button-box step3__button-box'}>
+                                <button className={'steps__submit'}>Wstecz</button>
+                                <button className={'steps__submit'}>Dalej</button>
+                            </div>
                     </form>
                 </div>
             </div>
